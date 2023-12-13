d448a5c8347769d81a412711a481b6116ae0e86f
Sándor Balázs <balazssandor0301@gmail.com>
2023-12-13 22:26:02 +0100

    day12 done

--- 2023/day12/day12.c
+++ 2023/day12/day12.c
@@ -13,13 +13,13 @@
 
 typedef
 struct {
-	char list[20];
+	char list[100];
 	int list_len;
 } CharList; 
 
 typedef
 struct {
-	int list[10];
+	int list[50];
 	int list_len;
 } IntList;
 
@@ -48,6 +48,14 @@ print_data(CharList arrangements[ROWS], IntList groups[ROWS]) {
 	}
 }
 
+void
+print_solution ( CharList arrangement, IntList group ) {
+	for ( int j = 0; j<arrangement.list_len; putc(arrangement.list[j++], stdout));
+	putc(' ', stdout);
+	for ( int j = 0; j<group.list_len; printf("%d", group.list[j++]));
+	putc('\n', stdout);
+}
+
 int
 intlist_sum(IntList intlist) {
 	int acc = 0, i = 0;
@@ -67,7 +75,7 @@ valid_solution( CharList arrangement, IntList groups ) {
 	int charlist_index = 0, group_size;
 	for ( int i = 0; i<groups.list_len; ++i ) {
 		for (; arrangement.list[charlist_index] == '.' && charlist_index < arrangement.list_len; charlist_index++);
-		if (charlist_index == arrangement.list_len-1) return 0;
+		if (charlist_index == arrangement.list_len) return 0;
 		group_size = 0;
 		for (group_size = 0; arrangement.list[charlist_index] == '#'
 							&& charlist_index < arrangement.list_len; group_size++, charlist_index++ );
@@ -77,15 +85,158 @@ valid_solution( CharList arrangement, IntList groups ) {
 }
 
 void
+charlist_cpy(const CharList src, CharList *dst) {
+	dst->list_len = src.list_len;
+	for ( int i = 0; i<src.list_len; ++i ) {
+		(dst->list)[i] = src.list[i];
+	}
+}
+
+void
+intlist_cpy(const IntList src, IntList *dst) {
+	dst->list_len = src.list_len;
+	for ( int i = 0; i<src.list_len; ++i ) {
+		(dst->list)[i] = src.list[i];
+	}
+}
+
+void
+charlist_five_times (CharList *charlist) {
+	int prev_len = charlist->list_len;
+	CharList tmp;
+	charlist_cpy(*charlist, &tmp);
+	charlist->list_len = 0;
+	for ( int i = 0; i<5; ++i) {
+		for ( int j = 0; j<prev_len; ++j) {
+			charlist->list[(charlist->list_len)++] = tmp.list[j];
+		}
+		charlist->list[(charlist->list_len)++] = '?';
+	}
+}
+
+void
+intlist_five_times (IntList *intlist) {
+	int prev_len = intlist->list_len;
+	IntList tmp;
+	intlist_cpy(*intlist, &tmp);
+	intlist->list_len = 0;
+	for ( int i = 0; i<5; ++i) {
+		for ( int j = 0; j<prev_len; ++j) {
+			intlist->list[(intlist->list_len)++] = tmp.list[j];
+		}
+		intlist->list[(intlist->list_len)++] = '?';
+	}
+}
+
+void
 part_1_solution( CharList arrangements[ROWS], IntList groups[ROWS] ) {
-	for ( int i = 0; i<ROWS; ++i ) {
-		int N, M, x, y, z, *p, *b; // for twiddling
+
+	int N, M, x, y, z, i, *p, *b; // for twiddling
+	unsigned long solutions = 0;
+	CharList tmp;
+
+	for ( int r = 0; r<ROWS; ++r ) {
+
+
 		// choosing M elements from N elements
-		M = intlist_sum(groups[i]) - char_sum(arrangements[i], '#');
-		N = char_sum(arrangements[i], '?');
+		M = intlist_sum(groups[r]) - char_sum(arrangements[r], '#');
+		N = char_sum(arrangements[r], '?');
+	
 		p = malloc( sizeof(int) * (N+2) );
 		b = malloc( sizeof(int) * N );
+
+		inittwiddle(M, N, p);
+
+		for(i = 0; i != N-M; i++) {
+			b[i] = 0;
+		}
+		while(i != N) {
+			b[i++] = 1;
+		}
+		
+		charlist_cpy(arrangements[r], &tmp);
+
+		for (int index = 0, i = 0; index < tmp.list_len; ++index) {
+			if (tmp.list[index] == '?') {
+				tmp.list[index] = (b[i++] ? '#' : '.');
+			}
+		}
+		if (valid_solution(tmp, groups[r])) solutions++;
+
+		while(!twiddle(&x, &y, &z, p))
+		{
+			charlist_cpy(arrangements[r], &tmp);
+
+			b[x] = 1;
+			b[y] = 0;
+			for (int index = 0, i = 0; index < tmp.list_len; ++index) {
+				if (tmp.list[index] == '?') {
+					tmp.list[index] = (b[i++] ? '#' : '.');
+				}
+			}
+			if (valid_solution(tmp, groups[r])) solutions++;
+		}
+
+		free(p);
+		free(b);
 	}
+	printf("Part 1 solution: %lu\n", solutions);
+}
+
+void
+part_2_solution( CharList arrangements[ROWS], IntList groups[ROWS] ) {
+
+	int N, M, x, y, z, i, *p, *b; // for twiddling
+	unsigned long solutions = 0;
+	CharList tmp;
+
+	for ( int r = 0; r<ROWS; ++r) {
+		charlist_five_times(&(arrangements[r]));
+		intlist_five_times(&(groups[r]));
+
+		// choosing M elements from N elements
+		M = intlist_sum(groups[r]) - char_sum(arrangements[r], '#');
+		N = char_sum(arrangements[r], '?');
+	
+		p = malloc( sizeof(int) * (N+2) );
+		b = malloc( sizeof(int) * N );
+
+		inittwiddle(M, N, p);
+
+		for(i = 0; i != N-M; i++) {
+			b[i] = 0;
+		}
+		while(i != N) {
+			b[i++] = 1;
+		}
+		
+		charlist_cpy(arrangements[r], &tmp);
+
+		for (int index = 0, i = 0; index < tmp.list_len; ++index) {
+			if (tmp.list[index] == '?') {
+				tmp.list[index] = (b[i++] ? '#' : '.');
+			}
+		}
+		if (valid_solution(tmp, groups[r])) solutions++;
+
+		while(!twiddle(&x, &y, &z, p))
+		{
+			charlist_cpy(arrangements[r], &tmp);
+
+			b[x] = 1;
+			b[y] = 0;
+			for (int index = 0, i = 0; index < tmp.list_len; ++index) {
+				if (tmp.list[index] == '?') {
+					tmp.list[index] = (b[i++] ? '#' : '.');
+				}
+			}
+			if (valid_solution(tmp, groups[r])) solutions++;
+		}
+
+		free(p);
+		free(b);
+	}
+	printf("Part 2 solution: %lu\n", solutions);
 }
 
 int
@@ -109,12 +260,8 @@ main(int argc, char *argv[]){
 		index++;
 	}
 
-	int max = 0;
-	for ( int i = 0; i<ROWS; ++i ) {
-		int sum = intlist_sum(groups[i]);
-		max = (max < sum ? sum : max);
-	}
-	printf("Max number of ? in an arrangement: %d\n");
+	part_1_solution(arrangements, groups);
+	// part_2_solution(arrangements, groups);
 
 	free(line);
 	fclose(fp);
